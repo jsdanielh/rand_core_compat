eaaa8b1801d95495f63680e7de481badbb22406f
Jose Daniel Hernandez <jd@nimiq.com>
2026-02-23 12:05:44 -0600

    Add support for rand_core 0.10

--- src/compat/mod.rs
+++ src/compat/mod.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "rand_core_0_10")]
+pub use v0_10::*;
 #[cfg(feature = "rand_core_0_5")]
 pub use v0_5::*;
 #[cfg(feature = "rand_core_0_6")]
@@ -5,6 +7,8 @@ pub use v0_6::*;
 #[cfg(feature = "rand_core_0_9")]
 pub use v0_9::*;
 
+#[cfg(feature = "rand_core_0_10")]
+mod v0_10;
 #[cfg(feature = "rand_core_0_5")]
 mod v0_5;
 #[cfg(feature = "rand_core_0_6")]
